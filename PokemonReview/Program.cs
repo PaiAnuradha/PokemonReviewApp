3680f6b769ebc8574cdd2981bb6040e702902515
Paianuradha <anuradhashenoy4@gmail.com>
2025-09-04 15:48:18 +0530

    Models, Relationships and DbContext

--- PokemonReview/Program.cs
+++ PokemonReview/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion("9.4.0")));
+
 
 var app = builder.Build();
 
